3b111df64da5a7eee2eb6c926990546ead89b547
ubuntu <belloaleksander@gmail.com>
2014-01-10 00:03:30 -0800

    shell: started main loop

---
+++ src/shell/mysh.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define MAX_LENGTH 500
+
+int main() {
+    while (1==1) {
+        char input[MAX_LENGTH];
+        printf("Enter cmd\n");
+        scanf("%s", input);
+        printf("%s\n", input);
+    }
+    return 1;
+}
