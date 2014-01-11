3c1293258d7d93f448671e99075a608b2f97981d
Archan Luhar <archan@caltech.edu>
2014-01-10 16:34:43 -0800

    shell: completed tokenizer

--- src/shell/mysh.c
+++ src/shell/mysh.c
@@ -10,23 +10,70 @@ struct command {
     char *stdout;
 };
 
-char ** tokenizer(char token, char * str) {
+char ** tokenizer(char delimiter, char * str) {
+    int num_tokens;
     char ** tokens;
-    int num_tokens = 0;
+
+    char * token;
+    int token_length;
     
-    while (*str != ASCII_NUL) {
-        if (*str == token) {
-            if (num_tokens == 0) {
-                num_tokens++;
+    int token_index;
+
+    char in_string_char;
+    char * str_it;
+
+    /*
+     * Count number of tokens.
+     */
+    in_string_char = ASCII_NUL;
+    num_tokens = 1;  /* Includes NULL terminal. */
+    str_it = str;
+    while (*str_it != ASCII_NUL) {
+        if (in_string_char) {  /* Inside a string */
+            if (*str_it == in_string_char) {  /* String ending */
+                in_string_char = ASCII_NUL;
             }
+        } else if (*str_it == '\'' || *str_it == '"') {  /* String starting */
+            in_string_char = *str_it;
+        } else if (*str_it == delimiter) {  /* Not in string delimiter */
             num_tokens++;
         }
         
-        str++;
+        str_it++;
+    }
+
+    /*
+     * Allocate token array.
+     */
+    tokens = (char **) malloc(sizeof(char *) * num_tokens);
+    tokens[num_tokens - 1] = NULL;
+    
+    /*
+     * Populate token array.
+     */
+    str_it = str;
+    token = str;
+    token_length = 0;
+    in_string_char = ASCII_NUL;
+    token_index = 0;
+    while (*str_it != ASCII_NUL) {
+        token_length++;
+
+        if (in_string_char) {
+            if (*str_it == in_string_char) {
+                in_string_char = ASCII_NUL;
+            }
+        } else if (*str_it == '\'' || *str_it == '"') {
+            in_string_char = *str_it;
+        } else if (*str_it == delimiter) {
+            tokens[i] = strndup(token, token_length);
+            token_index++;
+            token_length = 0;
+            token = str_it + 1;
+        }
+        
+        str_it++;
     }
     
-    /* num_tokens + 1 because tokens will be null terminated */
-    tokens = (char **) malloc(sizeof(char *) * (num_tokens + 1));
-    
-    // todo
+    return tokens;
 }
