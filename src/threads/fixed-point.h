cad7cc8b4f0d47ee7531e8b88888140819372203
Archan Luhar <archan@caltech.edu>
2014-02-05 19:03:04 -0800

    threads: comments to rest of fixed-point arithmetic functions

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -4,16 +4,21 @@
  * Use the type fixedpt for indicating a fixed-point variable.
  */
 
+
 /*! P.Q Fixed point arithmetic definitions. */
 #define FIXED_POINT_P 17
 #define FIXED_POINT_Q 14
-#define FIXED_POINT_F 1 << FIXED_POINT_Q
+
+/* Bit shift operation is probably evaluated at compile-time so as to have no
+   performance hit. */
+#define FIXED_POINT_F (1 << FIXED_POINT_Q)
+
 
 /*! A fixed point type is just an integer */
 typedef fixedpt int;
 
 
-
+/* Fixed point function prototypes */
 fixedpt int_to_fixedpt(int n);
 int fixedpt_to_int_zero(fixedpt x);
 int fixedpt_to_int_nearest(fixedpt x);
@@ -51,19 +56,22 @@ int fixedpt_to_int_nearest(fixedpt x) {
     }
 }
 
-
+/*! Adds two fixed-point numbers. */
 fixedpt fixedpt_add(fixedpt x, fixedpt y) {
     return x + y;
 }
 
+/*! Subtracts two fixed-point numbers. */
 fixedpt fixedpt_sub(fixedpt x, fixedpt y) {
     return x - y;
 }
 
+/*! Multiplies two fixed-point numbers. */
 fixedpt fixedpt_mul(fixedpt x, fixedpt y) {
     return ((int64_t) x) * y / FIXED_POINT_F;
 }
 
+/*! Divides two fixed-point numbers. */
 fixedpt fixedpt_div(fixedpt x, fixedpt y) {
     return ((int64_t) x) * FIXED_POINT_F / y;
 }
