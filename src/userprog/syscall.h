e3abc6286f15b5af97b030566390d1d3fe9f9661
Archan Luhar <archan@caltech.edu>
2014-02-09 17:11:57 -0800

    added rest of syscall comments and header prototypes

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,7 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+/* Installs the syscall handler into the interrupt vector table. */
 void syscall_init(void);
 
 /* Terminates Pintos by calling shutdown_power_off() (declared in 
@@ -34,6 +35,37 @@ bool remove(const char (file);
  */
 int open(const char *file);
 
+/* Returns the size, in bytes, of the file open as fd. */
+int filesize(int fd);
+
+/* Reads size bytes from the file open as fd into buffer. Returns the number
+ * of bytes actually read (0 at end of file), or -1 if the file could not be
+ * read (due to a condition other than end of file). Fd 0 reads from the
+ * keyboard using input_getc().
+ */
+int read(int fd, void *buffer, unsigned size);
+
+/* Writes size bytes from buffer to the open file fd. Returns the number of
+ * bytes actually written, which may be less than size if some bytes could not
+ * be written.
+ */
+int write(int fd, const void *buffer, unsigned size);
+
+/* Changes the next byte to be read or written in open file fd to position,
+ * expressed in bytes from the beginning of the file. (Thus, a position of 0
+ * is the file's start.)
+ */
+void seek(int fd, unsigned position);
+
+/* Returns the position of the next byte to be read or written in open file
+ * fd, expressed in bytes from the beginning of the file.
+ */
+unsigned tell(int fd);
+
+/* Closes file descriptor fd. Exiting or terminating a process implicitly
+ * closes all its open file descriptors, as if by calling this function for
+ * each one.
+ */
+ void close(int fd);
 
 #endif /* userprog/syscall.h */
-
