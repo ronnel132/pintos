89f863e7bdfd2053ce8f6850be93cd92842d721f
Archan Luhar <archan@caltech.edu>
2014-02-12 19:25:57 -0800

    stubs added for all syscalls

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -27,8 +27,7 @@ void halt (void) {
 
 /* Terminates the current user program, returning status to the kernel. */
 void exit(int status) {
-    
-
+    return;
 }
 
 /* Runs the executable whose name is given in cmd_line, passing any given 
@@ -38,19 +37,71 @@ pid_t exec(const char *cmd_line);
 
 /* Waits for a child process pid and retrieves the child's exit status. */
 int wait(pid_t pid) {
-    
+    return;
 }
 
 /* Creates a new file called file initially initial_size bytes in size. 
  * Returns true if successful, false otherwise. 
  */
-bool create(const char *file, unsigned initial_size);
+bool create(const char *file, unsigned initial_size) {
+	return false;
+}
 
 /* Deletes the file called file. Returns true if successful, false otherwise.
  */
-bool remove(const char (file);
+bool remove(const char (file) {
+	return false;
+}
 
 /* Opens the file called file. Returns a nonnegative integer handle called 
  * a "file descriptor" (fd), or -1 if the file could not be opened. 
  */
-int open(const char *file);
+int open(const char *file) {
+	return false;
+}
+
+
+/* Returns the size, in bytes, of the file open as fd. */
+int filesize(int fd) {
+	return false;
+}
+
+/* Reads size bytes from the file open as fd into buffer. Returns the number
+ * of bytes actually read (0 at end of file), or -1 if the file could not be
+ * read (due to a condition other than end of file). Fd 0 reads from the
+ * keyboard using input_getc().
+ */
+int read(int fd, void *buffer, unsigned size) {
+	return false;
+}
+
+/* Writes size bytes from buffer to the open file fd. Returns the number of
+ * bytes actually written, which may be less than size if some bytes could not
+ * be written.
+ */
+int write(int fd, const void *buffer, unsigned size) {
+	return false;
+}
+
+/* Changes the next byte to be read or written in open file fd to position,
+ * expressed in bytes from the beginning of the file. (Thus, a position of 0
+ * is the file's start.)
+ */
+void seek(int fd, unsigned position) {
+	return false;
+}
+
+/* Returns the position of the next byte to be read or written in open file
+ * fd, expressed in bytes from the beginning of the file.
+ */
+unsigned tell(int fd) {
+	return 0;
+}
+
+/* Closes file descriptor fd. Exiting or terminating a process implicitly
+ * closes all its open file descriptors, as if by calling this function for
+ * each one.
+ */
+ void close(int fd) {
+ 	return;
+ }
