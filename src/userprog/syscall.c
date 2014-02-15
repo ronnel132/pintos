188b3d240d1088263b24be89dd8335993da55197
Archan Luhar <archan@caltech.edu>
2014-02-14 16:33:32 -0800

    changed tabs to spaces

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -47,71 +47,71 @@ int wait(pid_t pid) {
  * Returns true if successful, false otherwise. 
  */
 bool create(const char *file, unsigned initial_size) {
-	bool status = false;
-	// TODO LOCK
-	status = filesys_create(file, initial_size);
-	// TODO UNLOCK
-	return status;
+    bool status = false;
+    // TODO LOCK
+    status = filesys_create(file, initial_size);
+    // TODO UNLOCK
+    return status;
 }
 
 /* Deletes the file called file. Returns true if successful, false otherwise.
  */
 bool remove(const char (file) {
-	bool status = false;
-	// TODO LOCK
-	status = filesys_remove(file, initial_size);
-	// TODO UNLOCK
-	return status;
+    bool status = false;
+    // TODO LOCK
+    status = filesys_remove(file, initial_size);
+    // TODO UNLOCK
+    return status;
 }
 
 /* Opens the file called file. Returns a nonnegative integer handle called 
  * a "file descriptor" (fd), or -1 if the file could not be opened. 
  */
 int open(const char *file) {
-	struct file * opened_file;
-	struct process_details * pd;
-	int file_descriptor = -1;
-	unsigned i;
+    struct file * opened_file;
+    struct process_details * pd;
+    int file_descriptor = -1;
+    unsigned i;
 
-	// TODO filesys LOCK
+    // TODO filesys LOCK
 
-	opened_file = filesys_open(file, initial_size);
+    opened_file = filesys_open(file, initial_size);
 
-	if (opened_file != NULL) {
-		pd = thread_current()->process_details;
+    if (opened_file != NULL) {
+        pd = thread_current()->process_details;
 
-		if (pd->num_files_open < MAX_OPEN_FILES) {
-			/* Search for first available file descriptor */
-			for (i = 0; i < MAX_OPEN_FILES; i++) {
-				if (cur_thread->process_details->open_file_descriptors[i] == false) {
-					file_descriptor = i;
-					break;
-				}
-			}
+        if (pd->num_files_open < MAX_OPEN_FILES) {
+            /* Search for first available file descriptor */
+            for (i = 0; i < MAX_OPEN_FILES; i++) {
+                if (cur_thread->process_details->open_file_descriptors[i] == false) {
+                    file_descriptor = i;
+                    break;
+                }
+            }
 
-			pd->open_file_descriptors[file_descriptor] = true;
-			pd->files[num_files_open] = opened_file;
-			pds->num_files_open++;
-		}
-	}
+            pd->open_file_descriptors[file_descriptor] = true;
+            pd->files[num_files_open] = opened_file;
+            pds->num_files_open++;
+        }
+    }
 
-	// TODO UNLOCK
-	return file_descriptor;
+    // TODO UNLOCK
+    return file_descriptor;
 }
 
 
 /* Returns the size, in bytes, of the file open as fd. */
 int filesize(int fd) {
-	int size = -1;
+    int size = -1;
 
-	// TODO: LOCK NEEDED OR NOT??
-	/* If file is indeed open, return length */
-	if (thread_current()->process_details->open_file_descriptors[fd]) {
-		size = inode_length(file_get_inode(pd->files[fd]));
-	}
-	// TODO UNLOCK
+    // TODO: LOCK NEEDED OR NOT??
+    /* If file is indeed open, return length */
+    if (thread_current()->process_details->open_file_descriptors[fd]) {
+        size = inode_length(file_get_inode(pd->files[fd]));
+    }
+    // TODO UNLOCK
 
-	return file_descriptor;
+    return file_descriptor;
 }
 
 /* Reads size bytes from the file open as fd into buffer. Returns the number
@@ -120,16 +120,16 @@ int filesize(int fd) {
  * keyboard using input_getc().
  */
 int read(int fd, void *buffer, unsigned size) {
-	struct file * f;
-	int bytes_read = -1;
+    struct file * f;
+    int bytes_read = -1;
 
-	// TODO LOCKS
-	if (file_is_open(fd)) {
-		bytes_read = file_read(get_file_struct(fd), buffer, size);
-	}
-	// TODO UNLOCK
+    // TODO LOCKS
+    if (file_is_open(fd)) {
+        bytes_read = file_read(get_file_struct(fd), buffer, size);
+    }
+    // TODO UNLOCK
 
-	return bytes_read;
+    return bytes_read;
 }
 
 /* Writes size bytes from buffer to the open file fd. Returns the number of
@@ -137,7 +137,7 @@ int read(int fd, void *buffer, unsigned size) {
  * be written.
  */
 int write(int fd, const void *buffer, unsigned size) {
-	return false;
+    return false;
 }
 
 /* Changes the next byte to be read or written in open file fd to position,
@@ -145,14 +145,14 @@ int write(int fd, const void *buffer, unsigned size) {
  * is the file's start.)
  */
 void seek(int fd, unsigned position) {
-	return false;
+    return false;
 }
 
 /* Returns the position of the next byte to be read or written in open file
  * fd, expressed in bytes from the beginning of the file.
  */
 unsigned tell(int fd) {
-	return 0;
+    return 0;
 }
 
 /* Closes file descriptor fd. Exiting or terminating a process implicitly
@@ -160,14 +160,14 @@ unsigned tell(int fd) {
  * each one.
  */
  void close(int fd) {
- 	return;
+    return;
  }
 
 /* Returns true if file descriptor refers to an open file */
 bool file_is_open(int fd) {
-	return thread_current()->process_details->open_file_descriptors[fd];
+    return thread_current()->process_details->open_file_descriptors[fd];
 }
 
 struct file * get_file_struct(int fd) {
-	return thread_current()->process_details->files[fd];
+    return thread_current()->process_details->files[fd];
 }
