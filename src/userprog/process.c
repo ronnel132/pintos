375daf693a7d2f12e3b05b54d552432bfbba51ac
Aleksander Bello <belloaleksander@gmail.com>
2014-02-17 07:29:08 -0800

    Now syscall works with bochs

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -83,6 +83,9 @@ static void start_process(void *file_name_) {
     This function will be implemented in problem 2-2.  For now, it does
     nothing. */
 int process_wait(tid_t child_tid UNUSED) {
+    for (;;) {
+        thread_yield();
+    }
     return -1;
 }
 
