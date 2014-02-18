f06554a78f6a6d4fad927bcc338905254a93280f
Aleksander Bello <belloaleksander@gmail.com>
2014-02-18 14:43:47 -0800

    Handling segfaults

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -79,7 +79,8 @@ static void kill(struct intr_frame *f) {
         printf("%s: dying due to interrupt %#04x (%s).\n",
                thread_name(), f->vec_no, intr_name(f->vec_no));
         intr_dump_frame(f);
-        thread_exit(); 
+//         thread_exit(); 
+        exit(-1);
 
     case SEL_KCSEG:
         /* Kernel's code segment, which indicates a kernel bug.
@@ -133,11 +134,6 @@ static void page_fault(struct intr_frame *f) {
     not_present = (f->error_code & PF_P) == 0;
     write = (f->error_code & PF_W) != 0;
     user = (f->error_code & PF_U) != 0;
-
-    /* User page fault is a segfault, for now */
-    if (user) {
-        exit(-1);
-    }
 
     /* To implement virtual memory, delete the rest of the function
        body, and replace it with code that brings in the page to
