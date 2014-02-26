bb9f790250fc2a4d89e067c49505c786bd77d331
Archan Luhar <archan@caltech.edu>
2014-02-25 17:31:01 -0800

    added todo

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -124,6 +124,8 @@ static void page_fault(struct intr_frame *f) {
        [IA32-v3a] 5.15 "Interrupt 14--Page Fault Exception (#PF)". */
     asm ("movl %%cr2, %0" : "=r" (fault_addr));
 
+    // TODO: Handle address that is allocated but not in physical memory
+
     /* Turn interrupts back on (they were only off so that we could
        be assured of reading CR2 before it changed). */
     intr_enable();
