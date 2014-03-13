7e6d33d6099a967f4ef9e6bf617105d42bc8d920
Ronnel Boettcher <ronnel132@gmail.com>
2014-03-13 02:08:01 -0700

    Added basic buffer cache implementation.

---
+++ src/filesys/cache.h
@@ -0,0 +1,36 @@
+#include <hash.h>
+#include <stdlib.h>
+#include "devices/block.h"
+#include "filesys/off_t.h"
+
+#define CACHE_SIZE 64
+
+/*! The mapping between filesys sector index and cache index. */
+struct hash cache_table; 
+
+/*! An entry in the filesystem buffer cache. */
+struct cache_entry {
+    /* The sector in the filesys block the cached block corresponds to. */
+    block_sector_t sector_idx;
+
+    void *data;
+    
+    /* Has this cached block been accessed? */
+    bool accessed;
+
+    /* Has this cached block been written to? */
+    bool dirty;
+
+    /*! The element stored in the hash table for the buffer cache. */
+    struct hash_elem elem;
+}; 
+
+void cache_init(void); 
+void cache_read(block_sector_t sector_idx, void *buffer, off_t size, 
+                off_t offset);
+void cache_write(block_sector_t sector_idx, void *buffer, off_t size,
+                 off_t offset);
+unsigned cache_hash(const struct hash_elem *element, void *aux);
+bool cache_less(const struct hash_elem *a, const struct hash_elem *b, 
+                void *aux);
+
